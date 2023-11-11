7d9547acb299b16ecfb838d6d48faed4091a44b3
редькина любовь <90209167+lyubabuba@users.noreply.github.com>
2023-11-11 21:06:09 +0300

    little fix for a better look

--- students/k33421/redkina_lyubov/lab2/labs/labs-env/conferences/conferences_app/views.py
+++ students/k33421/redkina_lyubov/lab2/labs/labs-env/conferences/conferences_app/views.py
@@ -1,110 +1,125 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView
 from .models import Author, Conference, Registration, Presentation, Review
-from .forms import PresentationForm, RegistrationForm, ReviewForm, AuthorForm
+from .forms import AuthorForm, RegistrationForm, PresentationForm, ReviewForm
 
-# Представление для отображения всех конференций
-def all_conferences(request):
-    conferences_list = Conference.objects.all()
-    return render(request, 'conferences.html', {'conferences': conferences_list})
+
+def home(request):
+    return render(request, 'home.html', {})
 
 def all_authors(request):
     authors_list = Author.objects.all()
     return render(request, 'authors.html', {'authors': authors_list})
 
+def author_detail(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    return render(request, 'author_detail.html', {'author': author})
+
+class AuthorCreateView(CreateView):
+    model = Author
+    form_class = AuthorForm
+    template_name = 'add_author.html'
+    success_url = '/'
+
+class AuthorUpdateView(UpdateView):
+    model = Author
+    form_class = AuthorForm
+    template_name = 'edit_author.html'
+    success_url = '/'
+
+
+
+
 def all_registrations(request):
     registrations_list = Registration.objects.all()
     return render(request, 'registrations.html', {'registrations': registrations_list})
 
-def all_presentations(request):
-    presentations_list = Presentation.objects.all()
-    return render(request, 'presentations.html', {'presentations': presentations_list})
+def registration_detail(request, registration_id):
+    registration = get_object_or_404(Registration, pk=registration_id)
+    return render(request, 'registration_detail.html', {'registration': registration})
+
+class RegistrationCreateView(CreateView):
+    model = Registration
+    form_class = RegistrationForm
+    template_name = 'add_registration.html'
+    success_url = '/'
+
+class RegistrationUpdateView(UpdateView):
+    model = Registration
+    form_class = RegistrationForm
+    template_name = 'edit_registration.html'
+    success_url = '/'
+
+class RegistrationDeleteView(DeleteView):
+    model = Registration
+    template_name = 'delete_registration.html'
+    success_url = '/'
+
+
+
 
 def all_reviews(request):
     reviews_list = Review.objects.all()
     return render(request, 'reviews.html', {'reviews': reviews_list})
 
-# Представление для отображения всех авторов для выбранной конференций
-def conference_authors(request, conference_id):
-    conference = get_object_or_404(Conference, pk=conference_id)
-    authors = Author.objects.filter(registration__conference=conference)
-    return render(request, 'conference_authors.html', {'conference': conference, 'authors': authors})
+def review_detail(request, review_id):
+    review = get_object_or_404(Review, pk=review_id)
+    return render(request, 'review_detail.html', {'review': review})
 
-# Представление для создания автора
-class AuthorCreateView(CreateView):
-    model = Author
-    form_class = AuthorForm
-    template_name = 'add_author.html'
-    success_url = reverse_lazy('authors_list')
-
-# Представление для редактирования автора
-class AuthorUpdateView(UpdateView):
-    model = Author
-    form_class = AuthorForm
-    template_name = 'edit_author.html'
-    success_url = reverse_lazy('authors_list')
-
-# Представление для создания презентации
-class PresentationCreateView(CreateView):
-    model = Presentation
-    form_class = PresentationForm
-    template_name = 'add_presentation.html'
-    success_url = reverse_lazy('presentations_list')
-
-# Представление для редактирования презентации
-class PresentationUpdateView(UpdateView):
-    model = Presentation
-    form_class = PresentationForm
-    template_name = 'edit_presentation.html'
-    success_url = reverse_lazy('presentations_list')
-
-# Представление для удаления презентации
-class PresentationDeleteView(DeleteView):
-    model = Presentation
-    template_name = 'delete_presentation.html'
-    success_url = reverse_lazy('presentations_list')
-
-# Представление для создания регистрации
-class RegistrationCreateView(CreateView):
-    model = Registration
-    form_class = RegistrationForm
-    template_name = 'add_registration.html'
-    success_url = reverse_lazy('registrarions_list')
-
-# Представление для редактирования регистрации
-class RegistrationUpdateView(UpdateView):
-    model = Registration
-    form_class = RegistrationForm
-    template_name = 'edit_registration.html'
-    success_url = reverse_lazy('registrarions_list')
-
-# Представление для удаления регистрации
-class RegistrationDeleteView(DeleteView):
-    model = Registration
-    template_name = 'delete_registration.html'
-    success_url = reverse_lazy('registrarions_list')
-
-# Представление для создания отзыва
 class ReviewCreateView(CreateView):
     model = Review
     form_class = ReviewForm
     template_name = 'add_review.html'
-    success_url = reverse_lazy('reviews_list')
+    success_url = '/'
 
-# Представление для редактирования отзыва
 class ReviewUpdateView(UpdateView):
     model = Review
     form_class = ReviewForm
     template_name = 'edit_review.html'
-    success_url = reverse_lazy('reviews_list')
+    success_url = '/'
 
-# Представление для удаления отзыва
-class ReviewDeleteView(DeleteView):
-    model = Review
-    template_name = 'delete_review.html'
-    success_url = reverse_lazy('reviews_list')
 
-# Представление для домашней страницы
-def home(request):
-    return render(request, 'home.html', {})
+
+
+def all_presentations(request):
+    presentations_list = Presentation.objects.all()
+    return render(request, 'presentations.html', {'presentations': presentations_list})
+
+def presentation_detail(request, presentation_id):
+    presentation = get_object_or_404(Presentation, pk=presentation_id)
+    return render(request, 'presentation_detail.html', {'presentation': presentation})
+
+class PresentationCreateView(CreateView):
+    model = Presentation
+    form_class = PresentationForm
+    template_name = 'add_presentation.html'
+    success_url = '/'
+
+class PresentationUpdateView(UpdateView):
+    model = Presentation
+    form_class = PresentationForm
+    template_name = 'edit_presentation.html'
+    success_url = '/'
+
+
+
+
+def all_conferences(request):
+    conferences_list = Conference.objects.all()
+    return render(request, 'conferences.html', {'conferences': conferences_list})
+
+def conference_detail(request, conference_id):
+    conference = get_object_or_404(Conference, pk=conference_id)
+    return render(request, 'conference_detail.html', {'conference': conference})
+
+def conference_authors(request, conference_id):
+    conference = get_object_or_404(Conference, pk=conference_id)
+    # Получаем все регистрации для данной конференции
+    registrations = Registration.objects.filter(conference=conference)
+    # Формируем словарь, где ключи - конференции, значения - списки авторов
+    authors_by_conference = {}
+    for registration in registrations:
+        if registration.conference not in authors_by_conference:
+            authors_by_conference[registration.conference] = []
+        authors_by_conference[registration.conference].append({'author': registration.author, 'title': registration.title})
+    return render(request, 'conference_authors.html', {'authors_by_conference': authors_by_conference})
